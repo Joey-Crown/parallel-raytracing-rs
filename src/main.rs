3cc1db83fb1d150c5a669e125f2f5a5cca5b1a1c
Joseph Crown <joe@thecrownfamily.com>
2024-02-06 17:36:01 -0500

    Cleaned up main with refactor

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::io::prelude::*;
+use image::{RgbImage, Rgb};
+use utils::vector::*;
+use utils::color::*;
+use utils::ray::*;
+use crate::utils::geometry::*;
+use crate::utils::camera::*;
+
+mod utils;
+
+fn main() {
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    let mut image_height = (image_width as f32 / aspect_ratio) as u32;
+    image_height = if image_height < 1 { 1 } else { image_height };
+
+    // World
+    let world = HittableList::new(vec![
+        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, Color::new(255,0,0))),
+        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, Color::new(0,255,0)))
+
+    ]);
+
+    // Camera
+    let camera = Camera::new();
+
+    // Render
+    camera.render(&world, image_width, image_height);
+
+}
---
+++ src/utils.rs
@@ -0,0 +1,5 @@
+pub mod vector;
+pub mod color;
+pub mod ray;
+pub mod geometry;
+pub mod camera;
